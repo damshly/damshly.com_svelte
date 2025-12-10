7baa85f238740f456892636174e40086225ddd4a
damshly <damshly@proton.me>
2025-12-11 02:48:26 +0400

    fix neoconfetti

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,9 @@ import { playwright } from '@vitest/browser-playwright';
 import { sveltekit } from '@sveltejs/kit/vite';
 
 export default defineConfig({
+	server: {
+    port: 3000
+  },	
 	plugins: [tailwindcss(), sveltekit()],
 	test: {
 		expect: { requireAssertions: true },
